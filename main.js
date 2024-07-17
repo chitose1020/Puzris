c567790177c90d72673d410b447792c537089c2a
chitose1020 <chitose1020@outlook.jp>
2024-07-17 23:30:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -30,10 +30,21 @@ let skin_id = 0;
 let Puz = 0;
 let Puz_x = 0;
 let Puz_y = 0;
+//スキンをロード/ロード完了時にゲームループ開始
+skin_load = 0;
+skin = [];
+  for(let i = 0; i < 8;i++){
+   skin[i] = new Image();
+   console.log(`skin/${skin_id}${i}.png`);
+   skin[i].src = `skin/${skin_id}${i}.png`;
+    skin[i].onload = () => {
+     skin_load++;
+    };
+  }
 //---------------------function---------------
 function draw_block(x,y,type){
   if(skin_load > 7){
-   ctx.drawImage(type,field_xx + x,field_yy + y);
+   ctx.drawImage(type,field_xx + x,field_yy + y,block_size,block_size);
   }
 }
 //---------------------Tick-------------------
@@ -55,9 +66,8 @@ function tick(){
  ctx.lineWidth = 5;
  ctx.strokeRect(x_half - block_size * 5 - 2, y_half - block_size * 9 - 2,block_size * 10 + 4,block_size * 20 + 4);
 //試しにブロック描画
-console.log(skin[1]);
   for(let i = 0; i < 8; i++){
-   draw_block(skin[i],0,block_size * i);
+   draw_block(0,block_size * i,skin[i]);
   }
 }
 //---------------------GameLoop----------------
@@ -77,17 +87,5 @@ function gameloop(){
  requestAnimationFrame(gameloop);
 }
 
-//スキンをロード/ロード完了時にゲームループ開始
-skin_load = 0;
-for(let i = 0; i < 8;i++){
- skin = [];
- skin[i] = new Image();
- skin[i].src = `skin/skin${skin_id}${i}.png`
- console.log(skin[i]);
-  skin[i].onload = (i) => {
-   skin_load++;
-   console.log("load");
-    }
-}
 console.log(skin[1]);
  requestAnimationFrame(gameloop);
